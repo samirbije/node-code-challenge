d1b896ec14777551a191b37bf2387bd08d08ac5c
samirbije <samirbije@gmail.com>
2020-11-05 22:43:02 +0900

    Create asset-manifest.json

---
+++ client/build/precache-manifest.6e80da9a5285a9724b43c2ffb6239c0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f255d09f8859af36a277eef305ce160c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e5b652ccfee721dff80",
+    "url": "/static/css/main.8bb97ed6.chunk.css"
+  },
+  {
+    "revision": "fa35b2fc6c6a149f4c34",
+    "url": "/static/js/2.e6a8916a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e6a8916a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e5b652ccfee721dff80",
+    "url": "/static/js/main.3dfa0574.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
